c8d4a367b2bb68c9ee494764b0b607db428e179f
Gayan Ssandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2016-05-10 17:36:21 +0530

    misc files added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gayan Sandaruwan
- * Date: 5/10/2016
- * Time: 2:10 PM
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <!-- Latest compiled and minified CSS -->
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+
+
+</head>
+<body>
+@include('includes.header')
+<div class="container">
+    @yield('contain')
+
+</div>
+</body>
+</html>
