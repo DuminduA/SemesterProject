a58ba597fc47401d65802a0e77f5fb77fd5d4fc4
Gayan Ssandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2016-05-12 23:34:47 +0530

    midifications for signup

--- app/Customer.php
+++ app/Customer.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 
 class Customer extends Model
 {
-    //
+    /**
+* The attributes that are mass assignable.
+*
+* @var array
+*/
+//    protected $fillable = [
+//        'name', 'email', 'password',
+//    ];
+//
+//    protected $hidden = [
+//        'password', 'remember_token',
+//    ];
 }
--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,8 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Customer;
 
 class CustomerController extends Controller
 {
-    //
+    public function postSignUp(Request $request){
+
+        $this->validate($request,[
+                
+
+        ]);
+
+        $first_name = $request['first_name'];
+        $last_name = $request['last_name'];
+        $email = $request['email'];
+        echo $email;
+        $phone = $request['phone'];
+        $password = bcrypt($request['password1']);
+//        $first_name = $request['first_name'];
+//        $first_name = $request['first_name'];
+        $customer = new Customer();
+        $customer->first_name=$first_name;
+        $customer->last_name =$last_name;
+        $customer->email=$email;
+        $customer->phone=$phone;
+        $customer->password=$password;
+
+        $customer->save();
+        return redirect()-> back();
+
+    }
+
 }
